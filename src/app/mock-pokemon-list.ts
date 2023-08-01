348071045b3d67491e5722e7287200aa3204d2e4
Laurent-PY <bichet.laurent@gmail.com>
2023-08-01 15:37:35 +0200

    FirstCommit

---
+++ src/app/mock-pokemon-list.ts
@@ -0,0 +1,112 @@
+import { Pokemon } from './pokemon';
+
+export const POKEMONS: Pokemon[] = [
+    {
+        id: 1,
+        name: "Bulbizarre",
+        hp: 25,
+        cp: 5,
+        picture: "https://www.pokepedia.fr/images/thumb/e/ef/Bulbizarre-RFVF.png/250px-Bulbizarre-RFVF.png",
+        types: ["Plante", "Poison"],
+        created: new Date()
+    },
+    {
+        id: 2,
+        name: "Salamèche",
+        hp: 28,
+        cp: 6,
+        picture: "https://www.pokepedia.fr/images/thumb/8/89/Salam%C3%A8che-RFVF.png/250px-Salam%C3%A8che-RFVF.png",
+        types: ["Feu"],
+        created: new Date()
+    },
+    {
+        id: 3,
+        name: "Carapuce",
+        hp: 21,
+        cp: 4,
+        picture: "https://www.pokepedia.fr/images/thumb/c/cc/Carapuce-RFVF.png/250px-Carapuce-RFVF.png",
+        types: ["Eau"],
+        created: new Date()
+    },
+    {
+        id: 4,
+        name: "Aspicot",
+        hp: 16,
+        cp: 2,
+        picture: "https://www.pokepedia.fr/images/thumb/9/9b/Aspicot-RFVF.png/250px-Aspicot-RFVF.png",
+        types: ["Insecte", "Poison"],
+        created: new Date()
+    },
+    {
+        id: 5,
+        name: "Roucool",
+        hp: 30,
+        cp: 7,
+        picture: "https://www.pokepedia.fr/images/thumb/9/94/Roucool-RFVF.png/250px-Roucool-RFVF.png",
+        types: ["Normal", "Vol"],
+        created: new Date()
+    },
+    {
+        id: 6,
+        name: "Rattata",
+        hp: 18,
+        cp: 6,
+        picture: "https://www.pokepedia.fr/images/thumb/9/9e/Rattata-RFVF.png/250px-Rattata-RFVF.png",
+        types: ["Normal"],
+        created: new Date()
+    },
+    {
+        id: 7,
+        name: "Piafabec",
+        hp: 14,
+        cp: 5,
+        picture: "https://www.pokepedia.fr/images/thumb/7/7d/Piafabec-RFVF.png/250px-Piafabec-RFVF.png",
+        types: ["Normal", "Vol"],
+        created: new Date()
+    },
+    {
+        id: 8,
+        name: "Abo",
+        hp: 16,
+        cp: 4,
+        picture: "https://www.pokepedia.fr/images/thumb/5/56/Abo-RFVF.png/250px-Abo-RFVF.png",
+        types: ["Poison"],
+        created: new Date()
+    },
+    {
+        id: 9,
+        name: "Pikachu",
+        hp: 21,
+        cp: 7,
+        picture: "https://www.pokepedia.fr/images/thumb/7/76/Pikachu-DEPS.png/250px-Pikachu-DEPS.png",
+        types: ["Electrik"],
+        created: new Date()
+    },
+    {
+        id: 10,
+        name: "Sabelette",
+        hp: 19,
+        cp: 3,
+        picture: "https://www.pokepedia.fr/images/thumb/6/66/Sabelette-RFVF.png/250px-Sabelette-RFVF.png",
+        types: ["Normal"],
+        created: new Date()
+    },
+    {
+        id: 11,
+        name: "Mélofée",
+        hp: 25,
+        cp: 5,
+        picture: "https://www.pokepedia.fr/images/thumb/4/4d/M%C3%A9lof%C3%A9e-RFVF.png/250px-M%C3%A9lof%C3%A9e-RFVF.png",
+        types: ["Fée"],
+        created: new Date()
+    },
+    {
+        id: 11,
+        name: "Nidorino",
+        hp: 21,
+        cp: 5,
+        picture: "https://www.pokepedia.fr/images/thumb/5/5f/Nidoking-RFVF.png/250px-Nidoking-RFVF.png",
+        types: ["Poisson"],
+        created: new Date()
+    }
+];
